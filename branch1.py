2776644841a0177951fca88e6df34d98f21298ab
see3dee <see3dee@gmail.com>
2018-09-04 22:13:19 -0400

    first files added

---
+++ branch1.py
@@ -0,0 +1,8 @@
+x =int(input('please enter a value to see if it is even or odd: '))
+if x%2 = 0:
+    print('')
+    print('x is even!!!')
+else:
+    print('')
+    print('x is odd!!')
+print('we are done here...')
---
+++ branch2.py
@@ -0,0 +1,7 @@
+x = int(input('please enter a value for x: '))
+y = int(input('please enter a value for y: '))
+z = int(input('please enter a value for z: '))
+
+if x < y and x < z:
+    print('x is the smallest value')
+    elif
